c474efc6e0db97748b83e5490816487280cf8ad9
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-10-30 11:03:44 +0530

    Prectice question added of loops

---
+++ Foundations/prectice-Question/loop-prectice.js
@@ -0,0 +1,69 @@
+//1️⃣ Print numbers from 1 to 10 using a loop.
+
+
+// for(let i = 1; i<=10 ; i++){
+//     console.log(i);
+    
+// }
+
+//1 2 3 4 5 6 7 8 9 10
+
+
+// 2️⃣ Print even numbers from 1 to 20.
+
+
+// for(let i = 1 ; i<=20 ; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+
+//2 4 6 8 ... 20
+
+
+// 3️⃣ Print all numbers divisible by 5 from 1 to 50.
+
+
+// for(let i= 1;i<=50;i++){
+//     if(i%5==0){
+//         console.log(i);
+        
+//     }
+// }
+
+// Output:5 10 15 20 ... 50
+
+
+// ✅ 4. Print a string 5 times
+
+// for(let i = 0 ; i<=5;i++){
+//     console.log("Hello world");
+// }
+
+
+
+// ✅ 5. Print numbers 10 → 1 (Reverse)
+
+// for(let i = 10 ; i>=1;i--){
+
+//     console.log(i);
+    
+// }
+
+
+//✅ 6. Sum of 1–100
+
+
+let sum = 0;
+
+for(let i =1;i<=100;i++){
+    sum = sum + i;
+    console.log(sum);
+    
+}
+
+
+console.log(sum);
+
+
+
