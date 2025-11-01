7e1d93d845b2637545e0490047fc8295577ac041
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-11-01 17:55:58 +0530

    Prectice question done

--- Foundations/prectice-Question/findSecondLagestNumberInArr.js
+++ Foundations/prectice-Question/findSecondLagestNumberInArr.js
@@ -1,25 +1,84 @@
 //wite a funtion that  retunr the second largest number in array 
 
 
-function secondRargest(arr){
-    let firstLargest = -Infinity;
-    let secondLargest = -Infinity;
+// function secondRargest(arr){
+//     let firstLargest = -Infinity;
+//     let secondLargest = -Infinity;
 
-    for(let i =0 ; i< arr.length;i++){
-        if(arr[i] > firstLargest){
-            secondLargest = firstLargest;
-            firstLargest = arr[i]
-        }
-        else if(arr[i] > secondLargest && arr[i] < firstLargest){
-            secondLargest = arr[i];
-        }
-    }
-    return secondLargest;
-}
+//     for(let i =0 ; i< arr.length;i++){
+//         if(arr[i] > firstLargest){
+//             secondLargest = firstLargest;
+//             firstLargest = arr[i]
+//         }
+//         else if(arr[i] > secondLargest && arr[i] < firstLargest){
+//             secondLargest = arr[i];
+//         }
+//     }
+//     return secondLargest;
+// }
 
 
-const arr = [20,10,40,30,10,20,90];
+// const arr = [20,10,40,30,10,20,90];
 
-let res = secondRargest(arr);
+// let res = secondRargest(arr);
 
-console.log(res);
+// console.log(res);
+
+
+// //also with the fin the third largest in the aray 
+
+// function thirdLargest(arr) {
+//     let firstLargest = -Infinity;
+//     let secondLargest = -Infinity;
+//     let thirdLargest = -Infinity;
+
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > firstLargest){
+//             thirdLargest = secondLargest;
+//             secondLargest = firstLargest;
+//             firstLargest = arr[i];
+//         } 
+//         else if(arr[i] > secondLargest && arr[i] < firstLargest){
+//             thirdLargest = secondLargest;
+//             secondLargest = arr[i];
+//         } 
+//         else if(arr[i] > thirdLargest && arr[i] < secondLargest){
+//             thirdLargest = arr[i];
+//         }
+//     }
+
+//     return thirdLargest;
+// }
+
+// const arr1 = [10, 20, 30];
+// let res1 = thirdLargest(arr1);
+// console.log(res1);
+
+
+
+//find the second largfest number in array
+
+
+
+// function findSecondLargestNumber(arr){
+//     let firstLargest = -Infinity;
+//     let secondLargest = -Infinity;
+
+
+//     for(let i = 0 ; i<arr.length;i++){
+//         if(arr[i] > firstLargest){
+//             secondLargest = firstLargest;
+//             firstLargest = arr[i]
+//         }
+//         else if(arr[i] > secondLargest && arr[i] < firstLargest){
+//             secondLargest = arr[i];
+//         }
+//     }
+//     return secondLargest
+// }
+
+// let arr = [10,30,20,50,80,90]
+
+// console.log(findSecondLargestNumber(arr));
+
+
---
+++ loopInLopp/firstLoop.js
@@ -0,0 +1,46 @@
+//loop in loop is called nested loop in programming
+//outer loop runs onece and innner looop run while the condition is not faild and its continices
+//exx
+
+
+
+// for(let i = 0 ; i< 3 ;i++){
+//     for(let j =0 ; j < 3 ; j++){
+//         console.log("i =" , i , "j =" , j)
+//     }
+// }
+
+
+
+
+
+    // for(let i =0 ; i<5 ; i++){
+    //     for(let j=0 ; j<=i ; j++){
+    //         console.log("i =" , i , "j =" , j)
+    //     }
+    // } 
+
+
+
+    // for(let i = 0 ; i < 3 ; i++){
+    //     for(let j = i ; j >0 ; j--){
+    //         console.log(i,j);
+            
+    //     }
+    // }
+
+
+
+    // for(let i = 0 ; i< 3 ; i++){
+    //     for(let j =i ; j >=0 ; j--){
+    //         console.log(i,j);
+            
+    //     }
+    // }
+
+    for(let i =5 ; i>0 ; i--){
+        for(let j = 0 ; j<i ; j++){
+            console.log(i,j);
+            
+        }
+    }
