690fda6fc86b38b08612263edba887924cd04d65
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-11-05 12:22:22 +0530

    Write a funtion that check the number is palidrome or not

--- isPalidrome.js
+++ isPalidrome.js
@@ -1,6 +1,8 @@
 // write a funtion that check the number is palifrome or not is palidrome so return trun nighter flase
 
  function isPalidrome(n){
+
+    if(n < 0) return false;
     let orignal = n;
     let reverse = 0;
 
@@ -15,5 +17,6 @@
     return false
  }
 
- console.log(isPalidrome(-121));
+//  console.log(isPalidrome(-121));
+ console.log(isPalidrome(323));
  
