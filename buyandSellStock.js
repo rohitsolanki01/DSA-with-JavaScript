ce1b937023f6f8e6e6642cc946cbd1cffaa2c60f
rohitsolanki01 <rohitsolanki0473@gmail.com>
2025-12-10 12:46:22 +0530

    solved the buy and sell stock leet code 121 easy questions

---
+++ buyandSellStock.js
@@ -0,0 +1,43 @@
+// ✅ Buy and Sell Stock – Best Time to Buy and Sell Stock (LeetCode 121)
+
+// Goal:
+// Given an array of stock prices, find the maximum profit you can achieve by buying on one day and selling on a later day.
+
+
+
+// 📘 Problem
+
+// You are given an array:
+
+// prices[i] = price of stock on day i
+
+
+// You must buy before you sell.
+
+// Return the maximum profit (or 0 if no profit is possible).
+
+
+function maxProfit(prices){
+    let minPrice = Infinity;
+    let maxProfit  = 0;
+
+
+    for(let i =0 ; i<prices.length;i++){
+        let price = prices[i];
+
+        if(price < minPrice){
+            minPrice = price
+        }
+
+        let profit = price - minPrice;
+
+        if(profit > maxProfit){
+            maxProfit = profit;
+        }
+    }
+    return maxProfit;
+}
+
+let prices = [7,1,5,3,6,4];
+
+console.log(maxProfit(prices))
--- reverseString.js
+++ reverseString.js
@@ -18,8 +18,6 @@
 // Output: ["h","a","n","n","a","H"]
 
 
-
-
 function reverseString(s){
     let len = s.length;
     let hlafLength = Math.floor(len /2);
@@ -39,4 +37,27 @@ const s2 = ["H","a","n","n","a","h"];
 console.log(reverseString(s2));
 
 const s3 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-console.log(reverseString(s3));
+console.log(reverseString(s3))
+
+
+
+
+
+// function revesedStringPrectice(s){
+//     let len = s.length;
+//     let hlafLength = len /2;    
+
+//     for(let i=0 ; i< hlafLength ; i++){
+//         let temp = s[i];
+//         s[i] = s[len -1-i];
+//         s[len-1-i] = temp;
+//     }
+
+//     return s;
+// }
+
+
+
+// let s1 = ["P" , "r" ,"i" , "i" , "y" , "a" , "a" , "a"];
+
+// console.log(revesedStringPrectice(s1));
