4def29e9470ea3ad04c9b7b9c0b2534c5db63272
rohitsolanki01 <rohitsolanki0473@gmail.com>
2026-02-24 17:23:14 +0530

    prectice questions

--- removeElement.js
+++ removeElement.js
@@ -20,59 +20,23 @@
 // You can only modify the original array
 
 
-// let removeElements = (arr,val) => {
-//     let x = 0 ; //for the track the postins  of the pointer for swaps 
+let removeElements = (arr,val) => {
+    let x = 0 ; //for the track the postins  of the pointer for swaps 
 
-//     for(let i =0 ; i < arr.length ; i++){
-//         if(arr[i] !== val){
-//             arr[x] = arr[i];
-//             x++
-//         }
-//     }
-//     return x;
-// }
-
-
-// let arr = [1,3,2,5,6,7,3,6,5,3,5,3,7,3];
-
-
-// console.log(removeElements(arr,3));
-
-
-
-function removeselementsFromAnArray(arr,val){
-    let x = 0 ;
-
-    for(let i =0 ; i< arr.length;i++){
-        if(arr[i] !== val){
-            arr[x] = arr[i];
-            x++;
+    for(let i =0 ; i < arr.length ; i++){
+        if(arr[i] !== val){ //check that val is not equl o number
+            arr[x] = arr[i]; //chage the positin of x
+            x++ //and with an done +x so x value be chaged to the next element 
         }
     }
-    return x;
+    return x; //return the x meas the index of the last element 
 }
 
 
-
-let arr = [1,2,3,4,5,2,8,2,9,2,10]
-console.log(removeselementsFromAnArray(arr,2));
+let arr = [1,3,2,5,6,7,3,6,5,3,5,3,7,3];
 
 
+console.log(removeElements(arr,3));
 
 
 
-function removesElemt(nums,val){
-    let x = 0;
-
-    for(let i = 0 ; i< nums.length ; i++){
-        if(nums[i] !== val){
-            nums[x] = nums[i];
-            x++;
-        
-        }
-    }
-    return x;
-}
-
-const arr2 = [1,2,3,4,5,2,8,2,9,2,10]
-console.log(removesElemt(arr2,2));
